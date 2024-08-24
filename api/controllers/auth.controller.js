9ec7a31978ab1ee9fe4cafd20da35938126c8336
AzizurDRahaman <89570145+AzizurDRahaman@users.noreply.github.com>
2024-08-24 13:50:56 +0530

    Created the Api route for Login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,3 +27,31 @@ export const signup = async (req, res) => {
             .json({ message: "Internal server error", error: err.message });
     }
 }
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+
+        if(!email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message: "Invalid credentials"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({message: "Invalid credentials"});
+        }
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+        res.status(200).json({message: "Login successful", user:{
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        }, token});
+    } catch(err){
+        res
+            .status(500)
+            .json({ message: "Internal server error", error: err.message });
+    }
+}
