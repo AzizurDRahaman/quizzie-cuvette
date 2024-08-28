e27089a3349fac45ef4f813b3162e93ce7672d2f
AzizurDRahaman <89570145+AzizurDRahaman@users.noreply.github.com>
2024-08-28 16:12:38 +0530

    Removed the JWT expiry time

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -43,7 +43,7 @@ export const login = async (req, res) => {
         if(!isMatch){
             return res.status(401).json({message: "Invalid credentials"});
         }
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
         res.status(200).json({message: "Login successful", user:{
             _id: user._id,
             name: user.name,
